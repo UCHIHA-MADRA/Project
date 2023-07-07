a8521edb20ab3bc5766a77ffc9ca5ddde2e1b314
UCHIHAMADRA <68822532+UCHIHA-MADRA@users.noreply.github.com>
2023-07-07 07:40:29 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+// Smooth scrolling
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+  
+      document.querySelector(this.getAttribute('href')).scrollIntoView({
+        behavior: 'smooth'
+      });
+    });
+  });
+  
+  // Active navigation highlighting
+  window.addEventListener('scroll', () => {
+    const sections = document.querySelectorAll('section');
+    const navLinks = document.querySelectorAll('nav ul li a');
+  
+    sections.forEach(section => {
+      const sectionTop = section.offsetTop;
+      const sectionHeight = section.clientHeight;
+      if (window.pageYOffset >= sectionTop - sectionHeight / 3) {
+        const sectionId = section.getAttribute('id');
+        navLinks.forEach(navLink => {
+          navLink.classList.remove('active');
+          if (navLink.getAttribute('href') === `#${sectionId}`) {
+            navLink.classList.add('active');
+          }
+        });
+      }
+    });
+  });
+  
